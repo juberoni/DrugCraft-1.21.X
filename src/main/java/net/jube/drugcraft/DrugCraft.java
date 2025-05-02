6f97ed5b1fc402f8e639c64162af5aee5379ae98
juberoni <maidajubeps@gmail.com>
2025-05-02 17:25:57 -0400

    Initial commit

---
+++ src/main/java/net/jube/drugcraft/DrugCraft.java
@@ -0,0 +1,16 @@
+package net.jube.drugcraft;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class DrugCraft implements ModInitializer {
+	public static final String MOD_ID = "drugcraft";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/jube/drugcraft/DrugCraftClient.java
@@ -0,0 +1,10 @@
+package net.jube.drugcraft;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DrugCraftClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
