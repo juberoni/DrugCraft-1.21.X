a3dc9dc54d4491c8949aedc189cd8e939d8242f0
juberoni <maidajubeps@gmail.com>
2025-05-02 17:27:48 -0400

    EXTREMELY IMPORTANT COMMENT

--- src/main/java/net/jube/drugcraft/DrugCraft.java
+++ src/main/java/net/jube/drugcraft/DrugCraft.java
@@ -5,6 +5,7 @@ import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+// I don't know what I'm doing.
 public class DrugCraft implements ModInitializer {
 	public static final String MOD_ID = "drugcraft";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
