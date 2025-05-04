a69475041781d72a2c9349e3ac7dacb907239256
juberoni <maidajubeps@gmail.com>
2025-05-03 21:41:07 -0400

    working on cropblock

---
+++ src/main/java/net/jube/drugcraft/block/custom/MarijuanaCropBlock.java
@@ -0,0 +1,14 @@
+package net.jube.drugcraft.block.custom;
+
+import net.minecraft.block.CropBlock;
+
+public class MarijuanaCropBlock extends CropBlock {
+    public static final int MAX_AGE = 4;
+
+    public MarijuanaCropBlock(Settings settings) {
+        super(settings);
+    }
+
+
+
+}
